5db6c29aa1b08c0a609bb9ec002fdac03bdb9059
Professor Usama <149573452+usamaamjid@users.noreply.github.com>
2023-12-09 02:48:42 +0500

    update 1 js

---
+++ 1.js
@@ -0,0 +1,17 @@
+// Question 1
+
+function a(){
+    console.log('A');
+}
+
+setTimeout(() =>{
+    console.log('B');
+}, 0)
+
+a();
+
+console.log('C');
+
+Promise.resolve().then(() =>{
+    console.log('D');
+});
